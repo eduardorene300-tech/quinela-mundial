e5caf5b5d88e8cb053943a6d678f6800ce832a28
eduardorene300-tech <eduardorene300@gmail.com>
2026-06-10 21:03:39 -0400

    Add initial app.py with game request and status

---
+++ app.py
@@ -0,0 +1,16 @@
+
+Request Game
+Couldn't assign game for logging: No available game found.
+Request Status
+Basketball-P1: Open
+Baseball-P1: Open
+Softball-P1: Open
+Filter by sport:
+Actions
+
+Last updated at 09:02:41
+
+Current Shift
+N/A
+Next Shift
+N/A
